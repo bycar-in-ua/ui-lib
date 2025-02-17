2b3275f1ddd39e926b0db93c46bdf2faaa39ab92
Vladyslav Bezsmertnyi <besmerch123@gmail.com>
2025-02-17 07:24:18 +0200

    initial

---
+++ src/icons/index.ts
@@ -0,0 +1,4 @@
+export * from "./interface";
+
+export { default as Coupe } from "./Coupe.vue";
+export { default as Drive } from "./Drive.vue";
---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import dts from "vite-plugin-dts";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue({ features: { propsDestructure: true } }),
+    dts({
+      tsconfigPath: "./tsconfig.app.json",
+    }),
+  ],
+  build: {
+    lib: {
+      entry: "src/index.ts",
+      formats: ["es"],
+      fileName: (_, entryName) => `${entryName}.js`,
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue",
+        },
+        preserveModules: true,
+      },
+    },
+  },
+});
